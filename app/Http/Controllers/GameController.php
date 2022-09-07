640dc4a8a58856a6da9dfb6773c18289ca00f75a
muoi.nguyenvanminh <lilo4801>
2022-09-07 14:42:41 +0700

    fix format code

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -27,52 +27,54 @@ class GameController extends Controller
         return view('game.create');
     }
 
-
     public function store(StoreGameRequest $request)
     {
         $request->validate();
         $image = '';
+
         if ($request->file('image')) {
             $file = $request->file('image');
             $filename = $file->getClientOriginalName();
+
             if (!file_exists(storage_path('public/image/game/' . $filename))) {
                 $file->move(public_path('image/game'), $filename);
             }
 
             $image = $filename;
         }
+
         $res = $this->gameService->create($request->input('title'), $image, Auth::guard('admin')->user()->id);
 
         return redirect()->route('games.create')->with('msg', $res['message']);
     }
 
-
     public function show($id)
     {
     }
-
 
     public function edit($id)
     {
         return view('game.edit')->with('game', $this->gameService->find($id));
     }
 
-
     public function update(Request $request, $id)
     {
         $image = '';
+
         if ($request->file('image')) {
             $file = $request->file('image');
             $filename = date('YmdHi') . $file->getClientOriginalName();
+
             if (!file_exists(storage_path('public/image/game/' . $filename))) {
                 $file->move(public_path('image/game'), $filename);
             }
+
             $image = $filename;
         }
+
         $res = $this->gameService->update($request->input('title'), $image, Auth::guard('admin')->user()->id, $id);
         return redirect()->route('games.edit', $id)->with('msg', $res['message']);
     }
-
 
     public function destroy($id)
     {
